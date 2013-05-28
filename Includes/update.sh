075af31ec54dc4340e188238f9783bef39d37965
Lucas Teske <lucas@teske.com.br>
2013-05-28 14:29:50 -0300

    Atualizacao no update.sh

--- Includes/update.sh
+++ Includes/update.sh
@@ -7,6 +7,10 @@ then
 else
 	touch .lock-update
 	python tilemaker.py #2>>1 1>> /dev/null
-	scp -r VIVO CLARO\ BR TIM OI root@10.0.5.20:/var/www/hstiles/
+	if	[ -d "VIVO" -o -d "CLARO BR" -o -d "TIM" -o -d "OI" ]
+	then
+		scp -r VIVO CLARO\ BR TIM OI root@10.0.5.20:/var/www/hstiles/
+	fi
+	rm -fr VIVO CLARO\ BR TIM OI
 	rm .lock-update
 fi
